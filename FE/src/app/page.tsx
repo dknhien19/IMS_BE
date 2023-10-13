0606931ecb6c09e06fd44fb6e6f64ac4d11c7d69
dknhien19 <dknhien2002@gmail.com>
2023-10-13 15:40:35 +0700

    fe

---
+++ FE/src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import HeaderComponent from "@/components/Header";
+import React from "react";
+
+const Home = () => (
+  <div className="App">
+    <HeaderComponent />
+  </div>
+);
+
+export default Home;
